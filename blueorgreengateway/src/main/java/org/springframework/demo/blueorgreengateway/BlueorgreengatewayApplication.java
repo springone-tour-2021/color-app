86bd748b98b40f2e2b2e44f335c1dd9b643d1030
Jonatan Ivanov <jonatan.ivanov@gmail.com>
2021-05-04 10:39:06 -0700

    Modify routing mechanism to align with kubernetes Service capabilities.

--- blueorgreengateway/src/main/java/org/springframework/demo/blueorgreengateway/BlueorgreengatewayApplication.java
+++ blueorgreengateway/src/main/java/org/springframework/demo/blueorgreengateway/BlueorgreengatewayApplication.java
@@ -2,14 +2,19 @@ package org.springframework.demo.blueorgreengateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.GatewayFilterSpec;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.cloud.gateway.route.builder.UriSpec;
 import org.springframework.cloud.loadbalancer.annotation.LoadBalancerClient;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.HashMap;
+import java.util.Collections;
+import java.util.List;
 import java.util.Map;
 
 @SpringBootApplication
@@ -24,18 +29,23 @@ public class BlueorgreengatewayApplication {
 	@Bean
 	public RouteLocator routeLocator(RouteLocatorBuilder builder) {
 		return builder.routes()
+				.route(p -> p.path("/blueorgreen").and().header("X-SC-LB-Hint", "nonpremium")
+						.filters(this::circuitBreaker)
+						.uri("lb://blueorgreen-nonpremium"))
 				.route(p -> p.path("/blueorgreen")
-						.filters(f -> f.circuitBreaker(c -> c.setFallbackUri("forward:/colorfallback")))
-						.uri("lb://blueorgreen"))
+						.filters(this::circuitBreaker)
+						.uri("lb://blueorgreen-premium"))
 				.route(p -> p.path("/").or().path("/color").or().path("/js/**").uri("lb://blueorgreenfrontend"))
 				.build();
 	}
 
+	private UriSpec circuitBreaker(GatewayFilterSpec gatewayFilterSpec) {
+		return gatewayFilterSpec.circuitBreaker(config -> config.setFallbackUri("forward:/colorfallback"));
+	}
+
 	@RequestMapping("/colorfallback")
 	public Map<String, String> fallbackColor() {
-		Map<String, String> map = new HashMap<>();
-		map.put("id", "red");
-		return map;
+		return Collections.singletonMap("id", "red");
 	}
 }
 
